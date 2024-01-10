2746a242874cbb5854125ca859d1384ee76fc983
Chekhanadski Andrei <chekhanadski@gmail.com>
2024-01-10 18:10:10 +0100

    feat: solve "getFalsyValuesCount" task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -413,8 +413,13 @@ function getElementByIndices(arr, indices) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.reduce((countFalsyValue, item) => {
+    if (!item) {
+      return countFalsyValue + 1;
+    }
+    return countFalsyValue;
+  }, 0);
 }
 
 /**
